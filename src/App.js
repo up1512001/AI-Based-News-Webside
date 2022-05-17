eadecc839adba6f5928c504e7309b4d138352b38
up1512001 <utsav.patel1215@outlook.com>
2022-05-17 23:53:32 +0530

    fetched news and shown then in card with details with out ui only card's

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React , {useEffect} from "react";
+import React , {useState,useEffect} from "react";
 
 import alanBtn from "@alan-ai/alan-sdk-web";
+
+import NewsCards from "./components/NewsCards/NewsCards";
+
 
 const alanKey = '03df53878c57c0ad13a338c9175affcc2e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () =>{
 
+    const [newsArticles,setNewsArticles] = useState([]);
+
+
 useEffect(()=>{
     alanBtn({
         key : alanKey,
-        onCommand : ({command,articals})=>{
-            if(command === 'newHeadlines'){
-                console.log(articals);
+        onCommand : ({command,articles})=>{
+            if (command === 'newHeadlines'){
+                setNewsArticles(articles);
             }
         }
     })
@@ -20,6 +26,7 @@ useEffect(()=>{
     return (
         <div>
             <h1>Alan AI News Webside</h1>
+            <NewsCards articles={newsArticles} />
         </div>
     );
 }
---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { Card, CardActions, CardActionArea, CardContent, CardMedia , Button, Typography } from '@material-ui/core'; 
+
+import useStyles from './styles.js';
+
+const NewsCard = ({ article : { description, publishedAt , source, title , url , urlToImage } , i})=>{
+
+    const classes = useStyles();
+
+    return (
+        <Card>
+            <CardActionArea>
+                <CardMedia className={classes.media} image={urlToImage || 'https://en.pimg.jp/065/609/665/1/65609665.jpg'}  />
+                <div>
+                    <Typography variant="body2" color='textSecondary' component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color='textSecondary' component="h2">{source.name}</Typography>
+                </div>
+                <Typography gutterBottom variant="h5" >{title}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions >
+                <Button size="small" color="primary" >Learn More</Button>
+                <Typography variant="h5" color="textSecondary" >{i+1}</Typography>
+            </CardActions>
+        </Card>
+    );
+
+}
+
+export default NewsCard;
